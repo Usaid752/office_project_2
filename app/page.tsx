3cb273e38ec402ea729b70a3f13a56d73786a370
usaid-jameel <ujameel752@gmail.com>
2026-04-13 20:09:09 +0500

    complete_ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-
-import HeroSection from "../components/HomePage/HeroSection";
-import DigitalEmpireSection from "../components/HomePage/DigitalEmpireSection";
-import NewsHighlightsSection from "../components/HomePage/NewsHighlightsSection";
-import WhyDukaandarSection from "../components/HomePage/WhyDukaandarSection";
-import HowItsWork from "../components/HomePage/HowitsWork";
-import Features from "../components/HomePage/Features";
-import WhyChooseUs from "../components/HomePage/WhyChooseUs";
-import FaqSection from "../components/HomePage/FaqSection";
-
+import HeroSection from "@/components/HomePage/HeroSection";
+import DigitalEmpireSection from "@/components/HomePage/DigitalEmpireSection";
+import NewsHighlightsSection from "@/components/HomePage/NewsHighlightsSection";
+import WhyDukaandarSection from "@/components/HomePage/WhyDukaandarSection";
+import HowItsWork from "@/components/HomePage/HowitsWork";
+import Features from "@/components/HomePage/Features";
+import WhyChooseUs from "@/components/HomePage/WhyChooseUs";
+import FaqSection from "@/components/HomePage/FaqSection";
 
 export default function HomePage() {
   return (
