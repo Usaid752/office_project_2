cce7af2661a27ed6d1b1f3887e4b10cb0a206182
usaid-jameel <ujameel752@gmail.com>
2026-04-13 20:26:49 +0500

    refactor import paths in page.tsx to use absolute paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import HeroSection from "./_components/HeroSection";
-import DigitalEmpireSection from "./_components/DigitalEmpireSection";
-import NewsHighlightsSection from "./_components/NewsHighlightsSection";
-import WhyDukaandarSection from "./_components/WhyDukaandarSection";
-import HowItsWork from "./_components/HowitsWork";
-import Features from "./_components/Features";
-import WhyChooseUs from "./_components/WhyChooseUs";
-import FaqSection from "./_components/FaqSection";
+import HeroSection from "@/app/_components/HeroSection";
+import DigitalEmpireSection from "@/app/_components/DigitalEmpireSection";
+import NewsHighlightsSection from "@/app/_components/NewsHighlightsSection";
+import WhyDukaandarSection from "@/app/_components/WhyDukaandarSection";
+import HowItsWork from "@/app/_components/HowitsWork";
+import Features from "@/app/_components/Features";
+import WhyChooseUs from "@/app/_components/WhyChooseUs";
+import FaqSection from "@/app/_components/FaqSection";
 
 export default function HomePage() {
   return (
